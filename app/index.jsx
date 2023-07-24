09de6a8adbda212ef6551fbbd954a9088928aa7d
iamsatar <abdisatar.mohamed@pixeledge.io>
2023-07-24 23:44:37 +0300

    feat: style splash screen

--- app/index.jsx
+++ app/index.jsx
@@ -6,18 +6,18 @@ import {
 export default function Page() {
   return (
     <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-        <Link href="/home" asChild>
-          <Pressable style={{
-            marginTop: 50, backgroundColor: '#ff4500', paddingVertical: 15, paddingHorizontal: 25, borderRadius: 5, width: 100, justifyContent: 'center', textAlign: 'center',
-          }}
-          >
-            <Text>Home</Text>
-          </Pressable>
-        </Link>
+      <View style={{ flex: 1, flexDirection: 'row', marginTop: 180 }}>
+
+        <Text style={styles.subtitle}>
+          FitTracker
+        </Text>
+        <Text style={styles.sub}>+</Text>
       </View>
+      <Link href="/home" asChild>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Get Started</Text>
+        </Pressable>
+      </Link>
     </View>
   );
 }
@@ -26,22 +26,56 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
     padding: 24,
+    backgroundColor: '#FF0006',
   },
   main: {
     flex: 1,
     justifyContent: 'center',
-    maxWidth: 960,
+    flexDirection: 'column',
     marginHorizontal: 'auto',
+    marginTop: 100,
   },
   title: {
     fontSize: 64,
     fontWeight: 'bold',
-    fontFamily: 'Inter_500Medium',
+    fontFamily: 'Poppins_500Medium',
+    color: '#fff',
   },
   subtitle: {
     fontSize: 36,
-    color: '#38434D',
-    fontFamily: 'Inter_500Medium',
+    color: '#fff',
+    marginBottom: 150,
+    marginTop: 100,
+    fontFamily: 'Poppins_500Medium',
+  },
+  sub: {
+    fontSize: 24,
+    color: '#fff',
+    marginBottom: 150,
+    marginTop: 100,
+    fontFamily: 'Poppins_400Regular',
+  },
+  button: {
+    fontSize: 36,
+    width: '100%',
+    backgroundColor: '#fff',
+    paddingVertical: 15,
+    paddingHorizontal: 25,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontFamily: 'Poppins_500Medium',
+    color: '#FF0006',
+
+  },
+  buttonText: {
+    fontSize: 16,
+
+    fontFamily: 'Poppins_500Medium',
+    color: '#FF0006',
+
   },
 });
