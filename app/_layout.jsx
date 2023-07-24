a840be22035398cb34d1cabece072290b68cc6d2
iamsatar <abdisatar.mohamed@pixeledge.io>
2023-07-24 23:41:09 +0300

    feat: use poppins as a default

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,32 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable camelcase */
 import {
   Slot,
-  SplashScreen
+  SplashScreen,
 } from 'expo-router';
 
-import { Inter_500Medium, useFonts } from '@expo-google-fonts/inter';
+import {
+  Poppins_100Thin,
+  Poppins_100Thin_Italic,
+  Poppins_200ExtraLight,
+  Poppins_200ExtraLight_Italic,
+  Poppins_300Light,
+  Poppins_300Light_Italic,
+  Poppins_400Regular,
+  Poppins_400Regular_Italic,
+  Poppins_500Medium,
+  Poppins_500Medium_Italic,
+  Poppins_600SemiBold,
+  Poppins_600SemiBold_Italic,
+  Poppins_700Bold,
+  Poppins_700Bold_Italic,
+  Poppins_800ExtraBold,
+  Poppins_800ExtraBold_Italic,
+  Poppins_900Black,
+  Poppins_900Black_Italic,
+  useFonts,
+} from '@expo-google-fonts/poppins';
+
 import { useEffect } from 'react';
 
 SplashScreen.preventAutoHideAsync();
@@ -12,7 +34,24 @@ SplashScreen.preventAutoHideAsync();
 export default function Layout() {
   // Load the font `Inter_500Medium`
   const [fontsLoaded] = useFonts({
-    Inter_500Medium,
+    Poppins_100Thin,
+    Poppins_100Thin_Italic,
+    Poppins_200ExtraLight,
+    Poppins_200ExtraLight_Italic,
+    Poppins_300Light,
+    Poppins_300Light_Italic,
+    Poppins_400Regular,
+    Poppins_400Regular_Italic,
+    Poppins_500Medium,
+    Poppins_500Medium_Italic,
+    Poppins_600SemiBold,
+    Poppins_600SemiBold_Italic,
+    Poppins_700Bold,
+    Poppins_700Bold_Italic,
+    Poppins_800ExtraBold,
+    Poppins_800ExtraBold_Italic,
+    Poppins_900Black,
+    Poppins_900Black_Italic,
   });
 
   useEffect(() => {
