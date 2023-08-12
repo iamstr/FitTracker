f48cbaaccdda52f166339bfc8b572b0129ec8533
iamsatar <abdisatar.mohamed@pixeledge.io>
2023-08-12 14:57:19 +0300

    code: add valid regex

---
+++ service/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/prefer-default-export */
+function isValidEmail(email) {
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return emailPattern.test(email);
+}
+
+export { isValidEmail };
