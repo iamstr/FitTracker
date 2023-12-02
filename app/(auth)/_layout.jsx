550a7856eeae513b41dc667aff4d2d6930cc8e68
satar <abdisatar.mohamed@pixeledge.io>
2023-12-02 13:45:20 +0300

    feat: add new stack navigation

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -3,9 +3,34 @@ import React from 'react';
 
 function Auth() {
   return (
-    <Stack>
+    <Stack screenOptions={{
+      headerStyle: {
+        backgroundColor: 'transparent',
+        elevation: 0, // remove shadow on Android
+        shadowOpacity: 0, // remove shadow on iOS
+      },
+      headerShadowVisible: false,
+      headerShown: false,
+    }}
+    >
       <Stack.Screen name="signup" options={{ headerShown: false }} />
       <Stack.Screen name="login" options={{ headerShown: false }} />
+      <Stack.Screen name="complete-signup" />
+      <Stack.Screen name="metrics" />
+      <Stack.Screen name="goal" />
+      <Stack.Screen
+        name="activity"
+        options={{
+          headerShown: false,
+          title: 'choose activity',
+          headerShadowVisible: false,
+          headerStyle: {
+            backgroundColor: 'transparent',
+            elevation: 0, // remove shadow on Android
+            shadowOpacity: 0, // remove shadow on iOS
+          },
+        }}
+      />
     </Stack>
   );
 }
