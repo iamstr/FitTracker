ebfae382dd5a9873902f5a6136e6e62595340f6c
satar <abdisatar.mohamed@pixeledge.io>
2023-12-04 14:34:09 +0300

    feat: wrap context signup stack

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,37 +1,41 @@
 import { Stack } from 'expo-router';
 import React from 'react';
+import UserContextProvider from '../../contexts/userContext';
 
 function Auth() {
   return (
-    <Stack screenOptions={{
-      headerStyle: {
-        backgroundColor: 'transparent',
-        elevation: 0, // remove shadow on Android
-        shadowOpacity: 0, // remove shadow on iOS
-      },
-      headerShadowVisible: false,
-      headerShown: false,
-    }}
-    >
-      <Stack.Screen name="signup" options={{ headerShown: false }} />
-      <Stack.Screen name="login" options={{ headerShown: false }} />
-      <Stack.Screen name="complete-signup" />
-      <Stack.Screen name="metrics" />
-      <Stack.Screen name="goal" />
-      <Stack.Screen
-        name="activity"
-        options={{
-          headerShown: false,
-          title: 'choose activity',
-          headerShadowVisible: false,
-          headerStyle: {
-            backgroundColor: 'transparent',
-            elevation: 0, // remove shadow on Android
-            shadowOpacity: 0, // remove shadow on iOS
-          },
-        }}
-      />
-    </Stack>
+    <UserContextProvider>
+
+      <Stack screenOptions={{
+        headerStyle: {
+          backgroundColor: 'transparent',
+          elevation: 0, // remove shadow on Android
+          shadowOpacity: 0, // remove shadow on iOS
+        },
+        headerShadowVisible: false,
+        headerShown: false,
+      }}
+      >
+        <Stack.Screen name="signup" options={{ headerShown: false }} />
+        <Stack.Screen name="login" options={{ headerShown: false }} />
+        <Stack.Screen name="complete-signup" />
+        <Stack.Screen name="metrics" />
+        <Stack.Screen name="goal" />
+        <Stack.Screen
+          name="activity"
+          options={{
+            headerShown: false,
+            title: 'choose activity',
+            headerShadowVisible: false,
+            headerStyle: {
+              backgroundColor: 'transparent',
+              elevation: 0, // remove shadow on Android
+              shadowOpacity: 0, // remove shadow on iOS
+            },
+          }}
+        />
+      </Stack>
+    </UserContextProvider>
   );
 }
 
